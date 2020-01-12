66c33449d59b9d55cc400b4cd46144599a29b391
Jakub Zimoń <jakub.zimon@gmail.com>
2020-01-12 11:04:38 +0100

    Add fixtures for product

--- src/Fixture/TagFixture.php
+++ src/Fixture/TagFixture.php
@@ -8,15 +8,13 @@ use Faker\Factory;
 use Websnacks\SyliusTagPlugin\Entity\TagInterface;
 use Sylius\Bundle\FixturesBundle\Fixture\AbstractFixture;
 use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
-use Sylius\Component\Core\Model\ChannelInterface;
+use Sylius\Component\Product\Repository\ProductRepositoryInterface;
 use Sylius\Component\Locale\Model\LocaleInterface;
 use Sylius\Component\Resource\Factory\FactoryInterface;
 use Sylius\Component\Resource\Repository\RepositoryInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
-use Symfony\Component\Finder\Finder;
-use Symfony\Component\HttpFoundation\File\File;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Websnacks\SyliusTagPlugin\Entity\TagsAwareInterface;
 
 final class TagFixture extends AbstractFixture
 {
@@ -41,6 +39,9 @@ final class TagFixture extends AbstractFixture
     /** @var OptionsResolver */
     private $optionsResolver;
 
+    /** @var ProductRepositoryInterface */
+    private $productRepository;
+
     /**
      * TagFixture constructor.
      * @param ObjectManager $objectManager
@@ -48,24 +49,29 @@ final class TagFixture extends AbstractFixture
      * @param RepositoryInterface $vendorRepository
      * @param ChannelRepositoryInterface $channelRepository
      * @param RepositoryInterface $localeRepository
+     * @param ProductRepositoryInterface $productRepository\
      */
     public function __construct(
         ObjectManager $objectManager,
         FactoryInterface $tagFactory,
         RepositoryInterface $vendorRepository,
         ChannelRepositoryInterface $channelRepository,
-        RepositoryInterface $localeRepository
+        RepositoryInterface $localeRepository,
+        ProductRepositoryInterface $productRepository
     ) {
         $this->objectManager = $objectManager;
         $this->tagFactory = $tagFactory;
         $this->vendorRepository = $vendorRepository;
         $this->channelRepository = $channelRepository;
         $this->localeRepository = $localeRepository;
+        $this->productRepository = $productRepository;
 
         $this->faker = Factory::create();
         $this->optionsResolver = (new OptionsResolver())
-            ->setRequired('tags_per_channel')
-            ->setAllowedTypes('tags_per_channel', 'int');
+            ->setRequired(['tags_per_product', 'tags_per_channel'])
+            ->setAllowedTypes('tags_per_product', 'int')
+            ->setAllowedTypes('tags_per_channel', 'int')
+            ;
     }
 
     public function load(array $options): void
@@ -73,6 +79,7 @@ final class TagFixture extends AbstractFixture
         $options = $this->optionsResolver->resolve($options);
 
         $channels = $this->channelRepository->findAll();
+        $tags = [];
         foreach ($channels as $channel) {
             for ($i = 1; $i < $options['tags_per_channel']; $i++) {
                 /** @var TagInterface $tag */
@@ -85,8 +92,16 @@ final class TagFixture extends AbstractFixture
                     $tag->setFallbackLocale($localeCode);
                     $tag->setName($this->faker->name);
                 }
-
+                $tags[] = $tag;
                 $this->objectManager->persist($tag);
+            }
+        }
+        $products = $this->productRepository->findAll();
+        $tagsPerProduct = $this->getProductTagsPartition($tags, count($products), $options['tags_per_product']);
+
+        foreach ($tagsPerProduct as $key => $productTags) {
+            foreach ($productTags as $tag) {
+                $tag->addProduct($products[$key]);
             }
         }
         $this->objectManager->flush();
@@ -97,6 +112,24 @@ final class TagFixture extends AbstractFixture
         return 'tag';
     }
 
+    protected function getProductTagsPartition(array $tagList, int $numberOfProducts, int $tagsPerProduct) : array
+    {
+        $numberOfTags = count($tagList);
+        $requiredTags = $numberOfProducts * $tagsPerProduct;
+        if ($numberOfTags < $requiredTags) {
+            $i = 0;
+            while (count($tagList) < $requiredTags) {
+                $tagList[] = $tagList[$i];
+                $i++;
+                $numberOfTags++;
+                if ($i >= $numberOfTags) {
+                    $i = 0;
+                }
+            }
+        }
+        return array_chunk($tagList, $tagsPerProduct);
+    }
+
     /**
      * {@inheritdoc}
      */
@@ -105,6 +138,7 @@ final class TagFixture extends AbstractFixture
         $optionsNode
             ->children()
             ->integerNode('tags_per_channel')->isRequired()->min(1)->end()
+            ->integerNode('tags_per_product')->isRequired()->min(1)->end()
         ;
     }
 
